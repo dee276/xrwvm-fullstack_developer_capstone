63feef64242d4c4e19a25ee45c87cf7ffa7d5a36
Dudley <dorestil@me.com>
2025-08-22 21:19:58 -0400

    lab5

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,9 +1,9 @@
 import Dealers from './components/Dealers/Dealers';
-import Dealer from "./components/Dealers/Dealer"
+import Dealer from "./components/Dealers/Dealer";
 import LoginPanel from "./components/Login/Login";
 import Register from "./components/Register/Register";
 import { Routes, Route } from "react-router-dom";
-import PostReview from "./components/Dealers/PostReview"
+import PostReview from "./components/Dealers/PostReview";
 
 function App() {
   return (
